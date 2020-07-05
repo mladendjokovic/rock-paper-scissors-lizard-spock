4fd98ea5ab7ee0cfa89063c5d07c125972e31269
mladendjokovic <mldndjkvc@gmail.com>
2020-07-05 23:35:52 +0200

    add main components

---
+++ src/components/Choice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Choice() {
+	return (
+		<div>
+			<div>
+				<img src="" alt="" />
+			</div>
+		</div>
+	);
+}
---
+++ src/components/ChoiceList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Choice from './Choice';
+
+export default function ChoiceList() {
+	return (
+		<div>
+			<Choice />
+			<div>
+				<Choice />
+				<Choice />
+			</div>
+			<div>
+				<Choice />
+				<Choice />
+			</div>
+		</div>
+	);
+}
